122b49d55384bf97195e22e7d130d09706c93903
Pedro Luz <pedromsluz@gmail.com>
2022-10-31 07:59:30 +0000

    Initial

---
+++ lib/ex_schemaorg.ex
@@ -0,0 +1,18 @@
+defmodule ExSchemaorg do
+  @moduledoc """
+  Documentation for `ExSchemaorg`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSchemaorg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_schemaorg_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSchemaorgTest do
+  use ExUnit.Case
+  doctest ExSchemaorg
+
+  test "greets the world" do
+    assert ExSchemaorg.hello() == :world
+  end
+end
